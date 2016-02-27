e5265d3a66eda07fd16bb098902a974edcd3a81f
Joe Strong <joestrong@gmail.com>
2016-02-27 15:54:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var Speakable = require('speakable');
+
+var speakable = new Speakable({key: 'AIzaSyB4DNzMrohRG0IBt_mBmm2cvnfbjKnpgCM'}, {
+  sox_path: 'sox',
+  threshold: '0.1'
+});
+
+speakable.on('speechStart', function() {
+  console.log('onSpeechStart');
+});
+
+speakable.on('speechStop', function() {
+  console.log('onSpeechStop');
+});
+
+speakable.on('speechReady', function() {
+  console.log('onSpeechReady');
+});
+
+speakable.on('error', function(err) {
+  console.log('onError:');
+  console.log(err);
+  speakable.recordVoice();
+});
+
+speakable.on('speechResult', function(recognizedWords) {
+  console.log('onSpeechResult:')
+  console.log(recognizedWords);
+  speakable.recordVoice();
+});
+
+speakable.recordVoice();
